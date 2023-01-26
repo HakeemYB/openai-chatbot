6a3c2fe3e2841eb0bd11301108c427e17f568848
HakeemYB <baighakeemyar.cloudlem@gmail.com>
2023-01-26 20:01:45 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { Configuration, OpenAIApi} from "openai";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const [prompt, setPrompt] = useState("");
+const [result, setResult] = useState("");
+const [loading, setLoading] = useState(false);
+
+const handleClick = async () => {
+  setLoading(true);
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.5,
+      max_tokens: 300,
+    });
+    setResult
+    (response.data.choices[0].text);
+  } catch (error) {
+    console.log(error);
+  }
+  setLoading(false);
+};
+
+return (
+  <main className="main">
+    <div className="auto">
+
+      <textarea
+        type="text"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        placeholder="Don't act smart, Think smart..."
+        className="textarea"
+        ></textarea>
+
+        <button
+        onClick={handleClick}
+        disabled={loading || prompt.length === 0}
+        className="btn"
         >
-          Learn React
-        </a>
-      </header>
+          {loading ? "Thinking...": "Generate"}
+        </button>
+
+        <pre className="result"><p>{result}</p></pre>
     </div>
-  );
+  </main>
+);
+
 }
 
 export default App;
